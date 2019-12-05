f6398dac3270d2d411ff1c5e30535eeba9a59d05
brohrer <brohrer@gmail.com>
2019-12-05 12:13:30 -0500

    version incremented

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 st.setup(
     name='lodgepole',
-    version='1',
+    version='3',
     description='A toolbox for image and video processing',
     url='http://github.com/brohrer/lodgepole',
     download_url='https://github.com/brohrer/lodgepole/tags/',
